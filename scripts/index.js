b6d6781c3d2e2eeef7cf876b0184c520f31e3b62
Alexander Zhukov <sanek-cska@yandex.ru>
2022-10-24 14:21:25 +0400

    Второе ревью

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,23 @@
 const popupElement = document.querySelector('.popup');
 const popupButtonCloseElement = popupElement.querySelector('.popup__button-close');
 const popupProfileEditbuttonElement = document.querySelector('.profile__editbutton');
+
+// Селектор элементов поп-апа
+const formElement = document.querySelector('.popup__container')
+let nameInput = formElement.querySelector('.popup__input_info_name')
+let jobInput = formElement.querySelector('.popup__input_info_job')
+const popupButtonSaveElement = formElement.querySelector('.popup__button-save');
+// Селектор элементов профиля
+const profilElement = document.querySelector('.profile');
+let nameProfil = profilElement.querySelector('.profile__name')
+let jobProfil = profilElement.querySelector('.profile__title')
+
+
 // Открытие-закрытие поп-апа
 const openPopup = function() {
     popupElement.classList.add('popup_is-opened');
+    nameInput.value = nameProfil.textContent;
+    jobInput.value = jobProfil.textContent;
 }
 const closePopup = function() {
     popupElement.classList.remove('popup_is-opened');
@@ -16,29 +30,16 @@ popupButtonCloseElement.addEventListener('click', closePopup);
 
 // Редактирование и перезапись полей имени и информации профиля
 
-// Селектор элементов поп-апа
-let formElement = document.querySelector('.popup__container')
-let nameInput = formElement.querySelector('.popup__name')
-let jobInput = formElement.querySelector('.popup__job')
-const popupButtonSaveElement = formElement.querySelector('.popup__button-save');
-// Селектор элементов профиля
-const profilElement = document.querySelector('.profile');
-let nameProfil = profilElement.querySelector('.profile__name')
-let jobProfil = profilElement.querySelector('.profile__title')
+
 
 // Функиця замены имени и информации профиля
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    if (nameInput.value && jobInput !== '') {
+    
         nameProfil.textContent = nameInput.value;
         jobProfil.textContent = jobInput.value;
         closePopup();
     }  
-    else {
-
-        closePopup();
-    }
-
-}
+   
 
 formElement.addEventListener('submit', formSubmitHandler); 
